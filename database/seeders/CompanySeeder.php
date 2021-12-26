ea025d25adf2b4068802e9d233e0e7f5359e544f
Danny Festor <danny.festor@gmail.com>
2021-12-26 13:54:21 +0900

    Add Company

---
+++ database/seeders/CompanySeeder.php
@@ -0,0 +1,827 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Company;
+use Illuminate\Database\Seeder;
+
+class CompanySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Company::create([
+            "id" => 1,
+            "name" => "ＪＲ",
+        ]);
+
+        Company::create([
+            "id" => 2,
+            "name" => "札幌市交通局",
+        ]);
+
+        Company::create([
+            "id" => 3,
+            "name" => "函館市企業局交通部",
+        ]);
+
+        Company::create([
+            "id" => 4,
+            "name" => "道南いさりび鉄道",
+        ]);
+
+        Company::create([
+            "id" => 5,
+            "name" => "津軽鉄道",
+        ]);
+
+        Company::create([
+            "id" => 6,
+            "name" => "弘南鉄道",
+        ]);
+
+        Company::create([
+            "id" => 7,
+            "name" => "ＩＧＲいわて銀河鉄道",
+        ]);
+
+        Company::create([
+            "id" => 8,
+            "name" => "青い森鉄道",
+        ]);
+
+        Company::create([
+            "id" => 9,
+            "name" => "三陸鉄道",
+        ]);
+
+        Company::create([
+            "id" => 10,
+            "name" => "仙台市交通局",
+        ]);
+
+        Company::create([
+            "id" => 11,
+            "name" => "阿武隈急行",
+        ]);
+
+        Company::create([
+            "id" => 12,
+            "name" => "仙台空港鉄道",
+        ]);
+
+        Company::create([
+            "id" => 13,
+            "name" => "秋田内陸縦貫鉄道",
+        ]);
+
+        Company::create([
+            "id" => 14,
+            "name" => "由利高原鉄道",
+        ]);
+
+        Company::create([
+            "id" => 15,
+            "name" => "山形鉄道",
+        ]);
+
+        Company::create([
+            "id" => 16,
+            "name" => "福島交通",
+        ]);
+
+        Company::create([
+            "id" => 17,
+            "name" => "会津鉄道",
+        ]);
+
+        Company::create([
+            "id" => 18,
+            "name" => "野岩鉄道",
+        ]);
+
+        Company::create([
+            "id" => 19,
+            "name" => "ひたちなか海浜鉄道",
+        ]);
+
+        Company::create([
+            "id" => 20,
+            "name" => "関東鉄道",
+        ]);
+
+        Company::create([
+            "id" => 21,
+            "name" => "鹿島臨海鉄道",
+        ]);
+
+        Company::create([
+            "id" => 22,
+            "name" => "真岡鐵道",
+        ]);
+
+        Company::create([
+            "id" => 23,
+            "name" => "首都圏新都市鉄道",
+        ]);
+
+        Company::create([
+            "id" => 24,
+            "name" => "東武鉄道",
+        ]);
+
+        Company::create([
+            "id" => 25,
+            "name" => "わたらせ渓谷鐵道",
+        ]);
+
+        Company::create([
+            "id" => 26,
+            "name" => "上毛電気鉄道",
+        ]);
+
+        Company::create([
+            "id" => 27,
+            "name" => "上信電鉄",
+        ]);
+
+        Company::create([
+            "id" => 28,
+            "name" => "西武鉄道",
+        ]);
+
+        Company::create([
+            "id" => 29,
+            "name" => "東京地下鉄",
+        ]);
+
+        Company::create([
+            "id" => 30,
+            "name" => "秩父鉄道",
+        ]);
+
+        Company::create([
+            "id" => 31,
+            "name" => "埼玉新都市交通",
+        ]);
+
+        Company::create([
+            "id" => 32,
+            "name" => "埼玉高速鉄道",
+        ]);
+
+        Company::create([
+            "id" => 33,
+            "name" => "京成電鉄",
+        ]);
+
+        Company::create([
+            "id" => 34,
+            "name" => "東京都交通局",
+        ]);
+
+        Company::create([
+            "id" => 35,
+            "name" => "流鉄",
+        ]);
+
+        Company::create([
+            "id" => 36,
+            "name" => "新京成電鉄",
+        ]);
+
+        Company::create([
+            "id" => 37,
+            "name" => "北総鉄道",
+        ]);
+
+        Company::create([
+            "id" => 38,
+            "name" => "山万",
+        ]);
+
+        Company::create([
+            "id" => 39,
+            "name" => "千葉都市モノレール",
+        ]);
+
+        Company::create([
+            "id" => 40,
+            "name" => "小湊鐵道",
+        ]);
+
+        Company::create([
+            "id" => 41,
+            "name" => "いすみ鉄道",
+        ]);
+
+        Company::create([
+            "id" => 42,
+            "name" => "銚子電気鉄道",
+        ]);
+
+        Company::create([
+            "id" => 43,
+            "name" => "東葉高速鉄道",
+        ]);
+
+        Company::create([
+            "id" => 44,
+            "name" => "舞浜リゾートライン",
+        ]);
+
+        Company::create([
+            "id" => 45,
+            "name" => "芝山鉄道",
+        ]);
+
+        Company::create([
+            "id" => 46,
+            "name" => "京王電鉄",
+        ]);
+
+        Company::create([
+            "id" => 47,
+            "name" => "小田急電鉄",
+        ]);
+
+        Company::create([
+            "id" => 48,
+            "name" => "東京急行電鉄",
+        ]);
+
+        Company::create([
+            "id" => 49,
+            "name" => "京浜急行電鉄",
+        ]);
+
+        Company::create([
+            "id" => 50,
+            "name" => "東京モノレール",
+        ]);
+
+        Company::create([
+            "id" => 51,
+            "name" => "ゆりかもめ",
+        ]);
+
+        Company::create([
+            "id" => 52,
+            "name" => "東京臨海高速鉄道",
+        ]);
+
+        Company::create([
+            "id" => 53,
+            "name" => "多摩都市モノレール",
+        ]);
+
+        Company::create([
+            "id" => 54,
+            "name" => "高尾登山電鉄",
+        ]);
+
+        Company::create([
+            "id" => 55,
+            "name" => "御岳登山鉄道",
+        ]);
+
+        Company::create([
+            "id" => 56,
+            "name" => "相模鉄道",
+        ]);
+
+        Company::create([
+            "id" => 57,
+            "name" => "横浜市交通局",
+        ]);
+
+        Company::create([
+            "id" => 58,
+            "name" => "横浜シーサイドライン",
+        ]);
+
+        Company::create([
+            "id" => 59,
+            "name" => "湘南モノレール",
+        ]);
+
+        Company::create([
+            "id" => 60,
+            "name" => "江ノ島電鉄",
+        ]);
+
+        Company::create([
+            "id" => 61,
+            "name" => "伊豆箱根鉄道",
+        ]);
+
+        Company::create([
+            "id" => 62,
+            "name" => "箱根登山鉄道",
+        ]);
+
+        Company::create([
+            "id" => 63,
+            "name" => "横浜高速鉄道",
+        ]);
+
+        Company::create([
+            "id" => 64,
+            "name" => "箱根ロープウェイ",
+        ]);
+
+        Company::create([
+            "id" => 65,
+            "name" => "北越急行",
+        ]);
+
+        Company::create([
+            "id" => 66,
+            "name" => "しなの鉄道",
+        ]);
+
+        Company::create([
+            "id" => 67,
+            "name" => "えちごトキめき鉄道",
+        ]);
+
+        Company::create([
+            "id" => 68,
+            "name" => "あいの風とやま鉄道",
+        ]);
+
+        Company::create([
+            "id" => 69,
+            "name" => "富山地方鉄道",
+        ]);
+
+        Company::create([
+            "id" => 70,
+            "name" => "黒部峡谷鉄道",
+        ]);
+
+        Company::create([
+            "id" => 71,
+            "name" => "万葉線",
+        ]);
+
+        Company::create([
+            "id" => 72,
+            "name" => "富山ライトレール",
+        ]);
+
+        Company::create([
+            "id" => 73,
+            "name" => "のと鉄道",
+        ]);
+
+        Company::create([
+            "id" => 74,
+            "name" => "北陸鉄道",
+        ]);
+
+        Company::create([
+            "id" => 75,
+            "name" => "ＩＲいしかわ鉄道",
+        ]);
+
+        Company::create([
+            "id" => 76,
+            "name" => "福井鉄道",
+        ]);
+
+        Company::create([
+            "id" => 77,
+            "name" => "えちぜん鉄道",
+        ]);
+
+        Company::create([
+            "id" => 78,
+            "name" => "富士急行",
+        ]);
+
+        Company::create([
+            "id" => 79,
+            "name" => "長野電鉄",
+        ]);
+
+        Company::create([
+            "id" => 80,
+            "name" => "上田電鉄",
+        ]);
+
+        Company::create([
+            "id" => 81,
+            "name" => "アルピコ交通",
+        ]);
+
+        Company::create([
+            "id" => 82,
+            "name" => "名古屋鉄道",
+        ]);
+
+        Company::create([
+            "id" => 83,
+            "name" => "長良川鉄道",
+        ]);
+
+        Company::create([
+            "id" => 84,
+            "name" => "樽見鉄道",
+        ]);
+
+        Company::create([
+            "id" => 85,
+            "name" => "明知鉄道",
+        ]);
+
+        Company::create([
+            "id" => 86,
+            "name" => "養老鉄道",
+        ]);
+
+        Company::create([
+            "id" => 87,
+            "name" => "伊豆急行",
+        ]);
+
+        Company::create([
+            "id" => 88,
+            "name" => "岳南電車",
+        ]);
+
+        Company::create([
+            "id" => 89,
+            "name" => "静岡鉄道",
+        ]);
+
+        Company::create([
+            "id" => 90,
+            "name" => "大井川鐵道",
+        ]);
+
+        Company::create([
+            "id" => 91,
+            "name" => "天竜浜名湖鉄道",
+        ]);
+
+        Company::create([
+            "id" => 92,
+            "name" => "遠州鉄道",
+        ]);
+
+        Company::create([
+            "id" => 93,
+            "name" => "近畿日本鉄道",
+        ]);
+
+        Company::create([
+            "id" => 94,
+            "name" => "豊橋鉄道",
+        ]);
+
+        Company::create([
+            "id" => 95,
+            "name" => "愛知環状鉄道",
+        ]);
+
+        Company::create([
+            "id" => 96,
+            "name" => "名古屋市交通局",
+        ]);
+
+        Company::create([
+            "id" => 97,
+            "name" => "東海交通事業",
+        ]);
+
+        Company::create([
+            "id" => 98,
+            "name" => "名古屋臨海高速鉄道",
+        ]);
+
+        Company::create([
+            "id" => 99,
+            "name" => "愛知高速交通",
+        ]);
+
+        Company::create([
+            "id" => 100,
+            "name" => "三岐鉄道",
+        ]);
+
+        Company::create([
+            "id" => 101,
+            "name" => "伊勢鉄道",
+        ]);
+
+        Company::create([
+            "id" => 102,
+            "name" => "伊賀鉄道",
+        ]);
+
+        Company::create([
+            "id" => 103,
+            "name" => "四日市あすなろう鉄道",
+        ]);
+
+        Company::create([
+            "id" => 104,
+            "name" => "京阪電気鉄道",
+        ]);
+
+        Company::create([
+            "id" => 105,
+            "name" => "近江鉄道",
+        ]);
+
+        Company::create([
+            "id" => 106,
+            "name" => "信楽高原鐵道",
+        ]);
+
+        Company::create([
+            "id" => 107,
+            "name" => "比叡山鉄道",
+        ]);
+
+        Company::create([
+            "id" => 108,
+            "name" => "阪急電鉄",
+        ]);
+
+        Company::create([
+            "id" => 109,
+            "name" => "叡山電鉄",
+        ]);
+
+        Company::create([
+            "id" => 110,
+            "name" => "京福電気鉄道",
+        ]);
+
+        Company::create([
+            "id" => 111,
+            "name" => "京都市交通局",
+        ]);
+
+        Company::create([
+            "id" => 112,
+            "name" => "嵯峨野観光鉄道",
+        ]);
+
+        Company::create([
+            "id" => 113,
+            "name" => "ＷＩＬＬＥＲ＿ＴＲＡＩＮＳ",
+        ]);
+
+        Company::create([
+            "id" => 114,
+            "name" => "南海電気鉄道",
+        ]);
+
+        Company::create([
+            "id" => 115,
+            "name" => "阪神電気鉄道",
+        ]);
+
+        Company::create([
+            "id" => 116,
+            "name" => "大阪市高速電気軌道",
+        ]);
+
+        Company::create([
+            "id" => 117,
+            "name" => "大阪高速鉄道",
+        ]);
+
+        Company::create([
+            "id" => 118,
+            "name" => "北大阪急行電鉄",
+        ]);
+
+        Company::create([
+            "id" => 119,
+            "name" => "泉北高速鉄道",
+        ]);
+
+        Company::create([
+            "id" => 120,
+            "name" => "阪堺電気軌道",
+        ]);
+
+        Company::create([
+            "id" => 121,
+            "name" => "水間鉄道",
+        ]);
+
+        Company::create([
+            "id" => 122,
+            "name" => "能勢電鉄",
+        ]);
+
+        Company::create([
+            "id" => 123,
+            "name" => "智頭急行",
+        ]);
+
+        Company::create([
+            "id" => 124,
+            "name" => "神戸高速",
+        ]);
+
+        Company::create([
+            "id" => 125,
+            "name" => "神戸電鉄",
+        ]);
+
+        Company::create([
+            "id" => 126,
+            "name" => "神戸市交通局",
+        ]);
+
+        Company::create([
+            "id" => 127,
+            "name" => "北神急行電鉄",
+        ]);
+
+        Company::create([
+            "id" => 128,
+            "name" => "神戸新交通",
+        ]);
+
+        Company::create([
+            "id" => 129,
+            "name" => "山陽電気鉄道",
+        ]);
+
+        Company::create([
+            "id" => 130,
+            "name" => "北条鉄道",
+        ]);
+
+        Company::create([
+            "id" => 131,
+            "name" => "紀州鉄道",
+        ]);
+
+        Company::create([
+            "id" => 132,
+            "name" => "和歌山電鐵",
+        ]);
+
+        Company::create([
+            "id" => 133,
+            "name" => "若桜鉄道",
+        ]);
+
+        Company::create([
+            "id" => 134,
+            "name" => "一畑電車",
+        ]);
+
+        Company::create([
+            "id" => 135,
+            "name" => "岡山電気軌道",
+        ]);
+
+        Company::create([
+            "id" => 136,
+            "name" => "水島臨海鉄道",
+        ]);
+
+        Company::create([
+            "id" => 137,
+            "name" => "井原鉄道",
+        ]);
+
+        Company::create([
+            "id" => 138,
+            "name" => "広島高速交通",
+        ]);
+
+        Company::create([
+            "id" => 139,
+            "name" => "広島電鉄",
+        ]);
+
+        Company::create([
+            "id" => 140,
+            "name" => "スカイレールサービス",
+        ]);
+
+        Company::create([
+            "id" => 141,
+            "name" => "錦川鉄道",
+        ]);
+
+        Company::create([
+            "id" => 142,
+            "name" => "阿佐海岸鉄道",
+        ]);
+
+        Company::create([
+            "id" => 143,
+            "name" => "高松琴平電気鉄道",
+        ]);
+
+        Company::create([
+            "id" => 144,
+            "name" => "伊予鉄道",
+        ]);
+
+        Company::create([
+            "id" => 145,
+            "name" => "とさでん交通",
+        ]);
+
+        Company::create([
+            "id" => 146,
+            "name" => "土佐くろしお鉄道",
+        ]);
+
+        Company::create([
+            "id" => 147,
+            "name" => "西日本鉄道",
+        ]);
+
+        Company::create([
+            "id" => 148,
+            "name" => "筑豊電気鉄道",
+        ]);
+
+        Company::create([
+            "id" => 149,
+            "name" => "北九州高速鉄道",
+        ]);
+
+        Company::create([
+            "id" => 150,
+            "name" => "平成筑豊鉄道",
+        ]);
+
+        Company::create([
+            "id" => 151,
+            "name" => "福岡市交通局",
+        ]);
+
+        Company::create([
+            "id" => 152,
+            "name" => "甘木鉄道",
+        ]);
+
+        Company::create([
+            "id" => 153,
+            "name" => "松浦鉄道",
+        ]);
+
+        Company::create([
+            "id" => 154,
+            "name" => "島原鉄道",
+        ]);
+
+        Company::create([
+            "id" => 155,
+            "name" => "長崎電気軌道",
+        ]);
+
+        Company::create([
+            "id" => 156,
+            "name" => "熊本電気鉄道",
+        ]);
+
+        Company::create([
+            "id" => 157,
+            "name" => "熊本市交通局",
+        ]);
+
+        Company::create([
+            "id" => 158,
+            "name" => "南阿蘇鉄道",
+        ]);
+
+        Company::create([
+            "id" => 159,
+            "name" => "くま川鉄道",
+        ]);
+
+        Company::create([
+            "id" => 160,
+            "name" => "肥薩おれんじ鉄道",
+        ]);
+
+        Company::create([
+            "id" => 161,
+            "name" => "鹿児島市交通局",
+        ]);
+
+        Company::create([
+            "id" => 162,
+            "name" => "沖縄都市モノレール",
+        ]);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,31 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
             UserSeeder::class,
+            PrefectureSeeder::class,
+            CitySeeder::class,
+            CompanySeeder::class,
         ]);
+
+        // Update ID index on PSQL...
+        $tables = \DB::select('SELECT table_name FROM information_schema.tables WHERE table_schema = \'public\' ORDER BY table_name;');
+
+// Set the tables in the database you would like to ignore
+        $ignores = array('password_resets');
+
+//loop through the tables
+        foreach ($tables as $table) {
+
+            // if the table is not to be ignored then:
+            if (!in_array($table->table_name, $ignores)) {
+
+                //Get the max id from that table and add 1 to it
+                $seq = \DB::table($table->table_name)->max('id') + 1;
+
+                // alter the sequence to now RESTART WITH the new sequence index from above
+                \DB::select('ALTER SEQUENCE ' . $table->table_name . '_id_seq RESTART WITH ' . $seq);
+
+            }
+
+        }
     }
 }
