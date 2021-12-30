7b3b758dc7e986abe55e9c72b15b3dfbb9151128
Danny Festor <danny.festor@gmail.com>
2021-12-30 09:51:58 +0900

    Update HandleInertiaRequests

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Middleware;
 
 class HandleInertiaRequests extends Middleware
@@ -34,9 +35,13 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request)
     {
         return array_merge(parent::share($request), [
-            'auth' => [
-                'user' => $request->user(),
-            ],
+            'auth' => Auth::user() ? [
+                'user' => [
+                    'username' => Auth::user()->name,
+                    'email' => Auth::user()->email,
+                ],
+                'can' => Auth::user()->permissions()->pluck('permissions.name')->toArray(),
+            ] : null
         ]);
     }
 }
