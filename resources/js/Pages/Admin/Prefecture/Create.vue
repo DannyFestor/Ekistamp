14e16f5638ea9759706b75597583fa9f66f6b5f5
Danny Festor <danny.festor@gmail.com>
2022-01-01 18:36:23 +0900

    Fix Design

--- resources/js/Pages/Admin/Prefecture/Create.vue
+++ resources/js/Pages/Admin/Prefecture/Create.vue
@@ -14,23 +14,23 @@
         Create New Prefecture
     </section>
 
-    <form @submit.prevent="storePrefecture" class="flex flex-col">
-        <section>
+    <form @submit.prevent="storePrefecture" class="flex flex-col space-y-4">
+        <section class="flex flex-col">
             <label for="kanji">漢字 Kanji</label>
             <input id="kanji" name="kanji" type="text" v-model="form.kanji">
             <div v-if="form.errors.kanji" v-text="form.errors.kanji" class="text-sm text-red-600"></div>
         </section>
-        <section>
+        <section class="flex flex-col">
             <label for="hiragana">ひらがな Hiragana</label>
             <input id="hiragana" name="hiragana" type="text" v-model="form.hiragana">
             <div v-if="form.errors.hiragana" v-text="form.errors.hiragana" class="text-sm text-red-600"></div>
         </section>
-        <section>
+        <section class="flex flex-col">
             <label for="katakana">カタカナ Katakana</label>
             <input id="katakana" name="katakana" type="text" v-model="form.katakana">
             <div v-if="form.errors.katakana" v-text="form.errors.katakana" class="text-sm text-red-600"></div>
         </section>
-        <section>
+        <section class="flex flex-col">
             <label for="romaji">ローマ字 Romaji</label>
             <input id="romaji" name="romaji" type="text" v-model="form.romaji">
             <div v-if="form.errors.romaji" v-text="form.errors.romaji" class="text-sm text-red-600"></div>
